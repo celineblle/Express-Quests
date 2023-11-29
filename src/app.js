a6b26d07c9c1f73e77f9c7051119060a626d0eaf
celineblle <celineblle@outlook.fr>
2023-11-29 10:58:23 +0100

    quete express 04

--- src/app.js
+++ src/app.js
@@ -16,5 +16,7 @@ app.post("/api/movies", validateMovie, movieControllers.postMovie);
 app.post("/api/users", validateUser, usersControllers.postUser);
 app.put("/api/movies/:id", validateMovie, movieControllers.updateMovie);
 app.put("/api/users/:id", validateUser, usersControllers.updateUser);
+app.delete("/api/movies/:id", movieControllers.deleteMovie);
+app.delete("/api/users/:id", usersControllers.deleteUser);
 
 module.exports = app;
